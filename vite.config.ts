6504534dceecafd6f5ecf31eb289b4bc19d484d2
Oleksii Solotvinskyi <Oleksii.Solotvinskyi@abrio.de>
2025-07-29 22:09:52 +0300

    fix: add vercel preset

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import { vercelPreset } from "@vercel/remix/vite";
+import { vitePlugin as remix } from "@remix-run/dev";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
+    vercelPreset(),
     remix({
       ignoredRouteFiles: ["**/*.css"],
     }),
