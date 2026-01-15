2be6e566f3d5d92b2af7f099a1e2ee6a0aad40e6
Oleksii Solotvinskyi <Oleksii.Solotvinskyi@abrio.de>
2026-01-15 22:21:17 +0200

    add: change route

--- app/routes/home.products.$productId.changes.tsx
+++ app/routes/home.products.$productId.changes.tsx
@@ -3,12 +3,24 @@ import { useState } from "react";
 import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 import invariant from "tiny-invariant";
 import { createChangeSet, getFilteredChangeSetsByProduct } from "~/server/changes.server";
+import ChangeSetCard from "~/components/ChangeSetCard";
+import { ChangeSetVM, NormDiff } from "~/types";
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   invariant(params.productId, "Missing productId param");
-  const pending = await getFilteredChangeSetsByProduct(params.productId, { createdAt: "desc" }, { status: "DRAFT" }, 0, 20);
-
-  return pending;
+  const pendingChanges = await getFilteredChangeSetsByProduct(params.productId, { createdAt: "desc" }, { status: "DRAFT" }, 0, 20);
+  // todo - tmp solution
+  const changeSetVMs = pendingChanges.flatMap((cs) =>
+    cs.diff
+      ? [
+          {
+            ...cs,
+            diff: cs.diff as NormDiff,
+          },
+        ]
+      : []
+  );
+  return changeSetVMs;
   // invariant(params.productId, "Missing productId param");
   // try {
   //   const { product, currentSnapshot } = await getProductWithNormsById(params.productId);
@@ -21,12 +33,20 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
   // }
 };
 
-export default function ProductNorm() {
+export default function NormChanges() {
   const loaderData = useLoaderData<typeof loader>();
   console.log("loaderData", loaderData);
   return (
     <>
-      <div className="edit-button__wrapper">changes</div>
+      <div className="edit-button__wrapper">
+        <ul className="list-unstyled">
+          {loaderData.map(({ id, status, createdAt, diff }) => (
+            <li key={id}>
+              <ChangeSetCard {...{ id, status, createdAt, diff }} />
+            </li>
+          ))}
+        </ul>
+      </div>
       <Outlet />
     </>
   );
