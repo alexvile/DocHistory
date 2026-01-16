6bccbeac6336961d9740f395e5dcf4003ae596a8
Oleksii Solotvinskyi <Oleksii.Solotvinskyi@abrio.de>
2026-01-16 19:18:21 +0200

    chore: remove unnecessary element

--- app/routes/home.products/route.tsx
+++ app/routes/home.products/route.tsx
@@ -1,14 +1,12 @@
 import { Outlet } from "@remix-run/react";
 
 import productsStyles from "~/styles/products.css?url";
-import extenderStyles from "~/components/Extender.css?url";
 import tableStyles from "~/components/ProductNormsTable.css?url";
 import lastChangedTooltipStyles from "~/components/LastChangedTooltip.css?url";
 
 export function links() {
   return [
     { rel: "stylesheet", href: productsStyles },
-    { rel: "stylesheet", href: extenderStyles },
     { rel: "stylesheet", href: tableStyles },
     { rel: "stylesheet", href: lastChangedTooltipStyles },
   ];
